062d9b27f611e554453262b36dc9fff765e578df
YKaushik12 <ky6633@srmist.edu.in>
2026-03-24 15:09:14 +0530

    uc2 commited yes

---
+++ app/src/usecase2.java
@@ -0,0 +1,15 @@
+public class usecase2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to BookMyStay Application");
+        System.out.println("Application   Started Successfully");
+
+        startApplication(); // calling UC1 method
+    }
+
+    // ✅ Use Case 1
+    static void startApplication() {
+        System.out.println("Hotel Booking System is Ready...");
+    }
+}
