ce213dfe6a25205c3a3bacbcc6d00fe56d74e245
YKaushik12 <ky6633@srmist.edu.in>
2026-03-24 15:37:42 +0530

    uc3 commited

---
+++ app/src/usecase3.java
@@ -0,0 +1,48 @@
+import java.util.HashMap;
+
+// ✅ Abstract Class (Abstraction)
+abstract class Booking {
+    String customerName;
+    int numberOfRooms;
+
+    Booking(String customerName, int numberOfRooms) {
+        this.customerName = customerName;
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    // Abstract method
+    abstract void calculateCost();
+
+    void displayDetails() {
+        System.out.println("Customer: " + customerName);
+        System.out.println("Rooms: " + numberOfRooms);
+
+
+         class usecase3 {
+
+            public static void main(String[] args) {
+
+                System.out.println("=== UC3: Centralized Inventory using HashMap ===");
+
+                // Creating HashMap for room availability
+                HashMap<String, Integer> roomInventory = new HashMap<>();
+
+                // Adding data
+                roomInventory.put("Single Room", 10);
+                roomInventory.put("Double Room", 5);
+                roomInventory.put("Suite", 2);
+
+                // Display all rooms
+                System.out.println("Room Availability:");
+                for (String roomType : roomInventory.keySet()) {
+                    System.out.println(roomType + " -> " + roomInventory.get(roomType));
+                }
+
+                // Booking a room (update value)
+                String bookedRoom = "Single Room";
+                roomInventory.put(bookedRoom, roomInventory.get(bookedRoom) - 1);
+
+                System.out.println("\nAfter Booking 1 Single Room:");
+                System.out.println(bookedRoom + " -> " + roomInventory.get(bookedRoom));
+            }
+        }
